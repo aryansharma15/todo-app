b409bdb6391508d3e5aef9c44df10dd171e8871f
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-02 16:15:52 +0530

    defined structure

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function Todo() {
+  return (
+    <div>
+        <input type="text" className="input" />
+        <button className="btn">Add</button>
+    </div>
+  )
+}
+
+export default Todo
