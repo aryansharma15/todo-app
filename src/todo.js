8cfd25965ca1f863e071e658f917d88b990a084f
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-02 16:40:21 +0530

    added functions and state

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css'
+import { useState } from 'react';
 
 function Todo() {
+
+    const [task, setTask] = useState('')
+    const [tasks, setTasks] = useState('')
+
+    const handleChange = (event) => {
+        setTask(event.target.value);
+        // console.log(task);
+    }
+
+    const addTodo = () => {
+        if(task !== ""){
+            setTasks([...tasks, task]);
+            setTask("");
+        }
+        console.log(tasks)
+    };
+
   return (
     <div>
-        <input type="text" className="input" />
-        <button className="btn">Add</button>
+        <input type="text" value={task} className="input" onChange={handleChange}/>
+        <button className="add-btn" onClick={addTodo}>Add</button>
     </div>
   )
 }
